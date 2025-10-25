4eec61364efee30d3ae570fcd590a7abd8e1895c
Abdullah-nder <carizma889@gmail.com>
2025-10-25 13:00:00 +0300

    frame work tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+    <div className='bg-amber-900' style={{ width: '200px', height: '200px' }}>
+      <h1 className='text-blue-500'>hello </h1>
+    </div>
+  )
+}
+
+export default App
